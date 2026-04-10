c69ddb552e56edeccd1e798b29cd69516087fcda
Jenward1978 <97267785+Jenward1978@users.noreply.github.com>
2026-04-10 10:22:52 -1100

    added a repo

--- TextBasedRPG/src/MainGame.java
+++ TextBasedRPG/src/MainGame.java
@@ -3,7 +3,7 @@ public class MainGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome");
+		System.out.println("Welcome to my first repo");
 
 	}
 
