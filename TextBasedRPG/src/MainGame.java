137d285cbbcb5fc61fa786ed478f9ccb802dbee8
Jenward1978 <97267785+Jenward1978@users.noreply.github.com>
2026-04-10 10:10:13 -1100

    added a print Welcome in main

--- TextBasedRPG/src/MainGame.java
+++ TextBasedRPG/src/MainGame.java
@@ -3,8 +3,7 @@ public class MainGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to my World");
-		System.out.println("test savvy");
+		System.out.println("Welcome");
 
 	}
 
