839558e8af6ecf9bc5ba8fadf4d2a6d1ccd77d69
Jenward Montalbo <jenwardmontalbo@pacific-horizons-school.com>
2026-04-10 00:01:25 -1100

    mdfd

--- TextBasedRPG/src/MainGame.java
+++ TextBasedRPG/src/MainGame.java
@@ -4,6 +4,7 @@ public class MainGame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to my World");
+		System.out.println("test");
 
 	}
 
