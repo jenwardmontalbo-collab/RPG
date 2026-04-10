28dc75b4973f758832dfeab9a88ffb36cf2170b1
Jenward1978 <97267785+Jenward1978@users.noreply.github.com>
2026-04-10 10:31:31 -1100

    Remove >>>

--- TextBasedRPG/src/MainGame.java
+++ TextBasedRPG/src/MainGame.java
@@ -3,14 +3,10 @@ public class MainGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("Welcome to my first repo");
-=======
 		System.out.println("Welcome");
 		System.out.println("This is me");
 		
->>>>>>> origin/main
-
 	}
 
 }
