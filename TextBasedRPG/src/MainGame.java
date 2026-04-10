96117227d573d215d72353dcfe88128ef1d0a5c6
Jenward Montalbo <jenwardmontalbo@pacific-horizons-school.com>
2026-04-10 00:04:24 -1100

    savvy

--- TextBasedRPG/src/MainGame.java
+++ TextBasedRPG/src/MainGame.java
@@ -4,7 +4,7 @@ public class MainGame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to my World");
-		System.out.println("test");
+		System.out.println("test savvy");
 
 	}
 
