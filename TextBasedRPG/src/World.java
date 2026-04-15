757d504562d8e0268c8fbd93f3876045719a5e28
daminpyo-cell <daminpyo@pacific-horizons-school.com>
2026-04-15 12:50:30 -1100

    deleted private variable name

--- TextBasedRPG/src/World.java
+++ TextBasedRPG/src/World.java
@@ -1,9 +1,7 @@
 
 public class World {
-	private String name;
 	private String description;
 	public World(String name, String description) {
-		this.name = name;
 		this.description = description;
 		//this is a comment made by Mr. Jen
 		
