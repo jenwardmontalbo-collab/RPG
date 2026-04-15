5705d0f8df4aff484944ea2547f9f5f1be506c2a
daminpyo-cell <daminpyo@pacific-horizons-school.com>
2026-04-15 12:59:25 -1100

    added class Type

--- TextBasedRPG/src/Type.java
+++ TextBasedRPG/src/Type.java
@@ -3,11 +3,8 @@ public class Type {
 	private String name;
 	private String description;
 	
-	
 	public Type(String name, String description) {
 		this.name = name;
 		this.description = description;
-		
 	}
-
 }
