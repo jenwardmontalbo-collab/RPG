8ba86f82ea4e94010399800a021b38cb20e2784b
Jenward1978 <97267785+Jenward1978@users.noreply.github.com>
2026-04-15 14:16:53 -1100

    edited MainGame.java

--- TextBasedRPG/src/MainGame.java
+++ TextBasedRPG/src/MainGame.java
@@ -2,11 +2,7 @@
 public class MainGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to my first repo");
-		System.out.println("Welcome");
-		System.out.println("This is me");
-		//Evan Zou was NOT here
+		
 	}
 
 }
