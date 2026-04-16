ad6a490de353a914484a4c53993b2f820ca09f74
daminpyo-cell <daminpyo@pacific-horizons-school.com>
2026-04-15 13:12:47 -1100

    added getters and setter methods to class Type

--- TextBasedRPG/src/Type.java
+++ TextBasedRPG/src/Type.java
@@ -1,11 +1,59 @@
 
+import java.util.Scanner;
 public class Type {
 	private String name;
 	private String description;
 	
-	public Type(String name, String description) {
-		this.name = name;
-		this.description = description;
-		//This comment was made in nano
+	public Type() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Choose your character type");
+		System.out.println("1. Archer");
+		System.out.println("2. Mage");
+		System.out.println("3. Knight");
+		this.name = input.next();
+		
+		if(name == "Archer") {
+			description = "Long ranged but weak damage";
+			
+		}
+		else if (name == "Mage") {
+			description = "High damage spells but low health";
+			
+		}
+		else if (name == "knight") {
+			description = "High damage and health, but low mobility";
+		}
+		
+		
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public void setName() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Choose your character type");
+		System.out.println("1. Archer");
+		System.out.println("2. Mage");
+		System.out.println("3. Knight");
+		this.name = input.next();
+		
+		if(name == "Archer") {
+			description = "Long ranged but weak damage";
+			
+		}
+		else if (name == "Mage") {
+			description = "High damage spells but low health";
+			
+		}
+		else if (name == "knight") {
+			description = "High damage and health, but low mobility";
+		}
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String descrip) {
+		this.description = descrip;
 	}
 }
