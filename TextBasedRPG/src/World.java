64a48743a26dc22d546a93d64886f64aa5b7fb82
Jenward1978 <97267785+Jenward1978@users.noreply.github.com>
2026-04-15 13:21:33 -1100

    added a new branch

--- TextBasedRPG/src/World.java
+++ TextBasedRPG/src/World.java
@@ -1,7 +1,9 @@
 
 public class World {
+	private String name;
 	private String description;
 	public World(String name, String description) {
+		this.name = name;
 		this.description = description;
 		//this is a comment made by Mr. Jen
 		
